a884326935ea286a1e1be0e133bc155a177b44e9
우동혁 <33048315+woodh0327@users.noreply.github.com>
2021-12-07 00:58:13 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const {User} = require('./models/User')
+
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://woodh0327:qwer1234@boilerplate.5yzj0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).json({success: true})
+    })
 })
 
 app.listen(port, () => {
