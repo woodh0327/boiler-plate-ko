c2c941b82c25ef054d15ebbc61675566d4030bd1
우동혁 <33048315+woodh0327@users.noreply.github.com>
2021-12-07 13:30:05 +0900

    혼자서 학습하기

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const {User} = require('./models/User')
 
 const config = require('./config/key')
@@ -21,8 +22,26 @@ app.get('/', (req, res) => {
 app.post('/register', (req, res) => {
     const user = new User(req.body)
     user.save((err, userInfo) => {
-        if (err) return res.json({success: false, err})
+        if (err)
+            return res.json({success: false, err})
         return res.status(200).json({success: true})
+    })
+})
+
+app.post('/login', (req, res) => {
+    User.findOne({email: req.body.email}, (err, user) => {
+        if (!user)
+            return res.json({loginSuccess: false, message: '제공된 이메일에 해당하는 유저가 없습니다.'})
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({loginSuccess: false, message: '비밀번호가 틀렸습니다.'})
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                res.cookie('x_auth', user.token)
+                .status(200)
+                .json({localSuccess: true, userId: user._id})
+            })
+        })
     })
 })
 
