129d84739d27f9829278d72ab064b1974da800e4
우동혁 <33048315+woodh0327@users.noreply.github.com>
2021-12-14 01:42:02 +0900

    로직정리 및 개선분 반영

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route, /* Link */} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, /* Link */} from 'react-router-dom';
 
 import LandingPage from './components/views/LandingPage/LandingPage'
 import LoginPage from './components/views/LoginPage/LoginPage';
